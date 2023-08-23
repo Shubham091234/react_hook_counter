a951219958998d9b8ca6e5dc06730abe1ec2ac24
Shubham091234 <shubhamsingh091234@gmail.com>
2023-08-24 01:52:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,33 @@ import "./index.css";
 
 const App = ()=> {
 
-  const [count, setCount] = useState(0);
+//   const [count, setCount] = useState(0);
 
-  const Add = ()=>{
+//   const Add = ()=>{
 
-    setCount(count +1);
-};
+//     setCount(count +1);
+// };
+
+
+let time=new Date().toLocaleTimeString();
+const [ctime, newtime] = useState(time);
+
+const Updatetime = ()=>{
+  let time=new Date().toLocaleTimeString();
+  newtime(time);
+}
+
+setInterval(Updatetime, 1000)
 
   return (
-<>
-<div className='main'>
+ <>
+{/* // <div className='main'>
   
-<h1 className='heading'> {count} </h1>
-<button onClick={Add} className='but'>Click Me</button>
+// <h1 className='heading'> {count} </h1>
+// <button onClick={Add} className='but'>Click Me</button>
 
-</div>
+// </div> */}
+<h1 className='time'>{time}</h1>
 </>
   ); 
   
